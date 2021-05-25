6a2338d56173d8b43e5827f2f29e3a14163bafa6
William NORIS <w.noris@protonmail.com>
2021-05-25 18:09:24 +0200

    first commit

---
+++ contact.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <?php
+        include("templates/header.html");
+    ?>
+    <body>
+        <div class="container">
+
+            <div class="bg-image"></div>
+
+            <?php
+                include("templates/body-header.php");
+            ?>
+
+            <div class="content">
+                <h1>Contact !!</h1>
+            </div>
+            
+            <?php
+                include("templates/body-footer.php");
+            ?>
+
+        </div>
+
+        
+    </body>
+</html>
---
+++ templates/body-footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+    Pied de page
+</div>
